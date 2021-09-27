366a13b0ca7c652100004b7e4ea37ccabf34575b
Imhootep <fpbraibant@gmail.com>
2021-09-27 11:33:44 +0200

    commit

--- client/src/components/profil/UpdateProfil.js
+++ client/src/components/profil/UpdateProfil.js
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { updateBio } from "../../actions/user.actions";
+import React, {
+  useState
+} from "react";
+import {
+  useSelector,
+  useDispatch
+} from "react-redux";
+import {
+  updateBio
+} from "../../actions/user.actions";
 import LeftNav from "../LeftNav";
 import UploadImg from "./UploadImg";
 
@@ -10,55 +17,91 @@ const UpdateProfil = () => {
   const userData = useSelector((state) => state.userReducer);
   const dispatch = useDispatch()
 
-  const handleUpdate =() =>{
+  const handleUpdate = () => {
     dispatch(updateBio(userData._id, bio))
     setUpdateForm(false)
   }
 
-  return (
-    <div className="profil-container">
-      <LeftNav />
-      <h1>Profil de {userData.pseudo}</h1>
-      <div className="update-container">
-        <div className="left-part">
-          <h3> Photo de profil</h3>
-          <img src={userData.picture} alt="profil Pic" />
-          <UploadImg />
-        </div>
-        <div className="right-part">
-          <div className="bio-update">
-            <h3>Profil</h3>
-            {updateForm === false && (
-              <>
-                <p>pseudo : {userData.pseudo}</p>
-                <p>email : {userData.email}</p>
-                <p onClick={() => setUpdateForm(!updateForm)}> Adresse : {userData.bio}</p>
-                <div>
-                
-                </div>
-                <button onClick={() => setUpdateForm(!updateForm)}>
-                  modifier profil
-                </button>
-              </>
-            )}
+  return ( <
+    div className = "profil-container" >
+    <
+    LeftNav / >
+    <
+    h1 > Profil de {
+      userData.pseudo
+    } < /h1> <
+    div className = "update-container" >
+    <
+    div className = "left-part" >
+    <
+    h3 > Photo de profil < /h3> <
+    img src = {
+      userData.picture
+    }
+    alt = "profil Pic" / >
+    <
+    UploadImg / >
+    <
+    /div> <
+    div className = "right-part" >
+    <
+    div className = "bio-update" >
+    <
+    h3 > Profil < /h3> {
+      updateForm === false && ( <
+        >
+        <
+        p > pseudo: {
+          userData.pseudo
+        } < /p> <
+        p > email: {
+          userData.email
+        } < /p> <
+        p onClick = {
+          () => setUpdateForm(!updateForm)
+        } > Adresse: {
+          userData.bio
+        } < /p> <
+        div >
 
-            {updateForm && (
-              <>
-                <textarea
-                  type="text"
-                  defaultValue={userData.bio}
-                  onChange={(e) => setBio(e.target.value)}
-                ></textarea>
-                <button onClick={handleUpdate}>
-                  Valider modification 
-                </button>
-              </>
-            )}
-          </div>
-          <h4> Membre depuis le : {userData.createdAt} </h4>
-        </div>
-      </div>
-    </div>
+        <
+        /div> <
+        button onClick = {
+          () => setUpdateForm(!updateForm)
+        } >
+        modifier profil <
+        /button> <
+        />
+      )
+    }
+
+    {
+      updateForm && ( <
+        >
+        <
+        textarea type = "text"
+        defaultValue = {
+          userData.bio
+        }
+        onChange = {
+          (e) => setBio(e.target.value)
+        } >
+        < /textarea> <
+        button onClick = {
+          handleUpdate
+        } >
+        Valider modification <
+        /button> <
+        />
+      )
+    } <
+    /div> <
+    h4 > Membre depuis le: {
+      userData.createdAt
+    } < /h4> <
+    /div> <
+    /div> <
+    /div>
   );
 };
 
