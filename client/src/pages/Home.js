81cfbbca18c835ebabd9dcef8e9a8a5737b6f2e3
Imhootep <fpbraibant@gmail.com>
2021-09-28 17:00:38 +0200

    posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import LeftNav from '../components/LeftNav';
+import React from "react";
+import Navbar from "../components/Navbar";
+import LeftNav from "../components/LeftNav";
+import Thread from "../components/Thread";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar/>
-            <div className="home">
-            <LeftNav/>
-            
+  return (
+    <div>
+      <Navbar />
+      <div className="home">
+        <LeftNav />
+        <div className="main">
+          <Thread />
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import userReducer from "./user.reducer";
 import usersReducer from "./users.reducer";
+import postReducer from "./post.reducer";
 
 export default combineReducers({
     userReducer,
     usersReducer,
+    postReducer,
     
 })
