3f9ff56c3babcb6202a79a45c19ce1ab62226cf2
Imhootep <fpbraibant@gmail.com>
2021-10-05 13:47:02 +0200

    roled users

--- client/src/reducers/users.reducer.js
+++ client/src/reducers/users.reducer.js
@@ -1,12 +1,14 @@
-import { GET_USERS } from "../actions/users.actions";
+import { GET_USERS, GET_ROLED_USERS } from "../actions/users.actions";
 
-const initialState=[];
+const initialState = [];
 
-export default function usersReducer(state=initialState, action) {
-    switch(action.type) {
-        case GET_USERS:
-            return action.payload;
-            default:
-                return state;
-    }
+export default function usersReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_USERS:
+      return action.payload;
+    case GET_ROLED_USERS:
+      return action.payload;
+    default:
+      return state;
+  }
 }
