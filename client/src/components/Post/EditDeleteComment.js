467ed4ede9860a68cf81fbbd3d1549eb9794a22c
Imhootep <fpbraibant@gmail.com>
2021-10-05 11:32:18 +0200

    edit comment post

--- client/src/components/Post/EditDeleteComment.js
+++ client/src/components/Post/EditDeleteComment.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { editComment } from "../../actions/post.actions";
+import { UidContext } from "../AppContext";
 
-const EditDeleteComment = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const EditDeleteComment = ({ comment, postId }) => {
+  const [isAuthor, setIsAuthor] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [text, setText] = useState("");
+  const uid = useContext(UidContext);
+  const dispatch = useDispatch()
+
+  const handleEdit = (e) => {
+      e.preventDefault();
+      if(text) {
+          dispatch (editComment(postId, comment._id, text))
+          setText('');
+          setEdit(false)
+          window.location.reload()
+      }
+  };
+
+  useEffect(() => {
+    const checkAuthor = () => {
+      if (uid === comment.commenterId) {
+        setIsAuthor(true);
+      }
+    };
+    checkAuthor();
+  }, [uid, comment.commenterId]);
+
+  return (
+    <div className="edit-comment">
+      {isAuthor && edit === false && (
+        <span onClick={() => setEdit(!edit)}>
+          <img src="./img/icons/edit.svg" alt="edit-comment" />
+        </span>
+      )}
+      {isAuthor && edit && (
+        <form action="" onSubmit={handleEdit} className="edit-comment-form">
+          <label htmlFor="edit" onClick={() => setEdit(!edit)}>
+            Editer
+          </label>
+          <br />
+          <input
+            type="text"
+            name="text"
+            onChange={(e) => setText(e.target.value)}
+            defaultValue={comment.text}
+          />
+          <br/>
+          <input type="submit" value="Valider modification"/>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default EditDeleteComment;
