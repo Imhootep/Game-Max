57bb6f5db75c8239b7dae6da90bb11d36f9190fe
Guillaume <guiguifr@msn.com>
2021-10-05 13:35:59 +0200

    ajout admin role vide

--- client/src/components/Administration.js
+++ client/src/components/Administration.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
+
 const Administration = () => {
 
     const users = useSelector((state) => state.usersReducer);
@@ -10,7 +11,19 @@ const Administration = () => {
             {users.map((val)=>{
             return(
                 <div className="adminBlock">
-                    {val.pseudo}: {val.role}
+                    <div className="adminSection">{val.pseudo}</div>
+                    <div className="adminSection">{val.role === '' ? 'ACCEPTER' : val.role}</div>
+                    <div className="adminSection">
+                        <select className="adminRoleSelect">
+                            <option value="studio" selected>Studio</option>
+                            <option value="expert">Expert</option>
+                            <option value="sponsor">Sponsor</option>
+                            <option value="partenaire">Partenaire</option>
+                        </select>
+                    </div>
+                    <div className="adminSection">
+                        <img src="../img/icons/add.svg" alt="add"/>
+                    </div>
                 </div>
                 )          
         })}
