f20c198fa0860ce426ae721e43c68d81a2acd5cf
Imhootep <fpbraibant@gmail.com>
2021-10-07 08:53:48 +0200

    new post fra

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // posts
 export const GET_POSTS = "GET_POSTS";
+export const ADD_POST = "ADD_POST";
 export const LIKE_POST = "LIKE_POST";
 export const UNLIKE_POST = "UNLIKE_POST";
 export const UPDATE_POST = "UPDATE_POST";
@@ -15,6 +16,9 @@ export const DELETE_COMMENT = "DELETE_COMMENT";
 
 
 
+export const GET_POST_ERRORS = "GET_POST_ERRORS";
+
+
 export const getPosts = (num) => {
   return (dispatch) => {
     return axios
@@ -24,6 +28,20 @@ export const getPosts = (num) => {
         dispatch({ type: GET_POSTS, payload: array });
       })
       .catch((err) => console.log(err));
+  };
+};
+
+export const addPost = (data) => {
+  return (dispatch) => {
+    return axios
+      .post(`${process.env.REACT_APP_API_URL}api/post/`, data)
+      .then((res) => {
+        if (res.data.errors) {
+          dispatch({ type: GET_POST_ERRORS, payload: res.data.errors });
+        } else {
+          dispatch({ type: GET_POST_ERRORS, payload: "" });
+        }
+      });
   };
 };
 
