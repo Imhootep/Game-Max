09192df7ff0f2835de81f69adfa1ea8bc87d60bb
Imhootep <fpbraibant@gmail.com>
2021-10-14 09:57:48 +0200

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 import { getUsers } from "./actions/users.actions";
-// import { getUser } from "./actions/user.actions";
+import { getUser } from "./actions/user.actions";
 
 // dev tools
 import { composeWithDevTools } from "redux-devtools-extension";
@@ -20,7 +20,7 @@ const store = createStore(
 //on fait un getUsers dès qu'on ouvre l'application
 store.dispatch(getUsers());
 store.dispatch(getPosts());
-// store.dispatch(getUser());
+store.dispatch(getUser());
 
 ReactDOM.render(
   <Provider store={store}>
