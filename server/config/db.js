b946549f287e18619577a7250fa6e38bb148cba8
alan-louis <louisalan95@hotmail.com>
2021-10-22 10:54:34 +0200

    Modif db.js alan

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+//mongodb+srv://"+ process.env.DB_USER_PASS +"@cluster0.sff55.mongodb.net/mern-project
 mongoose
   .connect(
     
-    "mongodb+srv://"+ process.env.DB_USER_PASS +"@cluster0.sff55.mongodb.net/mern-project",
+    "mongodb://" + process.env.DB_USER_PASS + "@cluster0-shard-00-00.sff55.mongodb.net:27017,cluster0-shard-00-01.sff55.mongodb.net:27017,cluster0-shard-00-02.sff55.mongodb.net:27017/" + process.env.DB_NAME + "?ssl=true&replicaSet=atlas-l2wiet-shard-0&authSource=admin&retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
