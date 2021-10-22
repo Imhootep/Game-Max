786e45ec14e79ced970d8c1e9080962ee103f94d
Flo-17 <cammarata.florian@hotmail.com>
2021-10-22 15:39:05 +0200

    update fonctionne pour company/pseudo

--- client/src/reducers/user.reducer.js
+++ client/src/reducers/user.reducer.js
@@ -15,6 +15,8 @@ export default function userReducer(state=initialState, action){
         case UPDATE_BIO:
             return {
                 ...state,
+                pseudo:action.payload.pseudo,
+                company:action.payload.company,
                 bio:action.payload.bio,
                 adresse:action.payload.adresse,
                 membres:action.payload.membres,
