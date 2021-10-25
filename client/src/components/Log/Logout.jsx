e5f9ffaacb5b4646b53e75cb67bb0ecfe6b9f470
Flo-17 <cammarata.florian@hotmail.com>
2021-10-25 14:21:55 +0200

    images bas droite s'affichent + clean logout

--- client/src/components/Log/Logout.jsx
+++ client/src/components/Log/Logout.jsx
@@ -6,16 +6,6 @@ import Cookies from 'js-cookie';
 const Logout = () => {
 
     const logout = async () =>{
-        // await axios({
-        //     method:"get",
-        //     headers : { Authorization : "Bearer "+Cookies.get('jwt') } ,
-        //     url:`${process.env.REACT_APP_API_URL}api/user/logout`,
-        //     withCredentials:true,
-        // })
-        // .then(()=> Cookies.set("jiwiti", "salut", { expires: 1 }))
-        // .catch((err)=> console.log(err))
-
-        // window.location = "/";
         Cookies.remove('jwt', { path: '' })
         window.location = "/";
     };
