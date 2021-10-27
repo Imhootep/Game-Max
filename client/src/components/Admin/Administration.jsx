51cca3d8d5a42c37f9aa054b909f40b86f27364c
Flo-17 <cammarata.florian@hotmail.com>
2021-10-27 12:27:04 +0200

    update via admin ok

--- client/src/components/Admin/Administration.jsx
+++ client/src/components/Admin/Administration.jsx
@@ -161,7 +161,7 @@ const Administration = () => {
         return axios({
             method:"patch",
             headers : { Authorization : "Bearer "+Cookies.get('jwt') },
-            url: `${process.env.REACT_APP_API_URL}api/user/role/` + id,
+            url: `${process.env.REACT_APP_API_URL}api/user/admin/update/` + id,
             data: {role:roleUser,adresse:adressUser}
           }).then(response => {
             setRefreshData(refreshData+1)
@@ -184,7 +184,7 @@ const Administration = () => {
         return axios({
             method:"patch",
             headers : { Authorization : "Bearer "+Cookies.get('jwt') },
-            url: `${process.env.REACT_APP_API_URL}api/user/role/` +id,
+            url: `${process.env.REACT_APP_API_URL}api/user/admin/update/` +id,
             data: {role:role}
           }).then(response => {
             setRefreshData(refreshData+1)
