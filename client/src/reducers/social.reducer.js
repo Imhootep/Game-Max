de1272b1fc26eb297cbe16b49179d0bf07b9f3f4
Guillaume <guiguifr@msn.com>
2021-10-27 08:52:38 +0200

    un peu de css

--- client/src/reducers/social.reducer.js
+++ client/src/reducers/social.reducer.js
@@ -2,7 +2,7 @@ import { GET_SOCIAL } from "../actions/user.actions";
 
 const initialState = {};
 
-export default function trendingReducer(state = initialState, action) {
+export default function trendingReducer(state = initialState, action) { // probleme
   switch (action.type) {
     case GET_SOCIAL:
       return action.payload;
