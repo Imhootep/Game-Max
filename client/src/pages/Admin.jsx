fcfdae53677ae694491df48cd2c2e2840ee503a0
Flo-17 <cammarata.florian@hotmail.com>
2021-11-02 10:00:38 +0100

    Update photo profil :d

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,19 +1,29 @@
-import React, { useContext  } from 'react';
+import React, { useEffect, useState, useContext  } from 'react';
+import { useSelector, useDispatch  } from 'react-redux';
 import { UidContext } from '../components/AppContext';
 import Navbar from '../components/Navigation/Navbar';
 import LeftNav from '../components/Navigation/LeftNav';
 import Administration from '../components/Admin/Administration';
+import { getUser} from "../actions/user.actions";
+
 import loading from '../img/loading.gif';
 import Cookies from 'js-cookie';
 import { Redirect } from 'react-router';
 
 const Admin = () => {
     const uid = useContext(UidContext)
-    const urole = useContext(UidContext)
+    const user = useSelector((state) => state.userReducer);
+    const dispatch = useDispatch ();
+    useEffect(()=>{
+            dispatch(getUser())
+            console.log("user sur admin :")
+            console.log(user)    
+            console.log("uid : ",uid) 
+        }, [])
 
     return (
         <>
-        {uid !== null && urole ? 
+        {uid !== null && user.isAdmin ?
         <div>
             <Navbar/>
             <div className="admin-container">
