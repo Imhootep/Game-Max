31ce8058e3994a30967bb6c436208b3f4ec83e1b
Guillaume <guiguifr@msn.com>
2021-11-09 10:21:05 +0100

    admin mis a jour toutes les 30 secs

--- client/src/components/Admin/Administration.jsx
+++ client/src/components/Admin/Administration.jsx
@@ -32,6 +32,13 @@ const Administration = () => {
     useEffect(()=>{
         dispatch(getUsers())
     }, [refreshData])
+
+    
+    //permet de lancer la requete toutes les 30 secs pr mettre a jour l'admin
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{ setRefreshData(refreshData+1) }, 30000)
+        return () => clearInterval(intervalId);
+    }, [])
     
 
     const [role,setRole] = useState("Studio"); //add user
@@ -50,8 +57,6 @@ const Administration = () => {
     
     // useeffect qui rempli bien les données, maintenant il faut le dl en csv
     useEffect(()=>{
-        console.log("longueur")
-        console.log(users.length)
         
 
         if(csvData.length === 0){
@@ -59,9 +64,11 @@ const Administration = () => {
                 // console.log(users[i].email)
                 setCsvData(oldArray => [...oldArray, [users[i].pseudo, users[i].email]]); 
             }
-        }else{
-            console.log("CSV deja rempli !")
         }
+
+        // else{
+        //     console.log("CSV deja rempli !")
+        // }
     }, [users])
 
     // role d'un non validé
